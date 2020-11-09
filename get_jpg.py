88761d627561ae815ec33a68ea1fb5ca2239c86c
hika019 <ikazuti2001@gmail.com>
2020-11-09 12:29:44 +0900

    update

--- get_jpg.py
+++ get_jpg.py
@@ -10,3 +10,10 @@ def get_jpg(file_name = 'test.jpg'):
     cmd = 'raspistill -o '+file_name
     returncode = subprocess.call(cmd, shell=True)
     return returncode
+
+def main():
+    returncode = get_jpg()
+    if(returncode != 0):
+        print("画像の撮影ができませんでした")
+    else:
+        print("画像の撮影に成功")
